03b2cf56d489f91e0c41514b6e37b06d437a71bc
xmarre <54859656+xmarre@users.noreply.github.com>
2026-08-11 12:27:29 +0200

    Register input library management routes

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 import logging
 
 from .image_conveyor import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .image_conveyor_library_ops import register_library_routes
 from .image_conveyor_server import register_routes
 
 WEB_DIRECTORY = "./web"
 
 try:
     register_routes()
+    register_library_routes()
 except Exception:
     logging.getLogger(__name__).exception(
         "Image Conveyor: failed to register HTTP routes; input-folder browsing and imports will be unavailable."
