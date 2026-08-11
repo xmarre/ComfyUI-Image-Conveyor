c0923d3a560f4171de88f5253c5db57d48774d89
xmarre <54859656+xmarre@users.noreply.github.com>
2026-08-11 14:02:33 +0200

    Register batch drag and materialization routes

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import logging
 
 from .image_conveyor import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .image_conveyor_drag_ops import register_drag_routes
 from .image_conveyor_library_ops import register_library_routes
 from .image_conveyor_server import register_routes
 
@@ -9,6 +10,7 @@ WEB_DIRECTORY = "./web"
 try:
     register_routes()
     register_library_routes()
+    register_drag_routes()
 except Exception:
     logging.getLogger(__name__).exception(
         "Image Conveyor: failed to register HTTP routes; input-folder browsing and imports will be unavailable."
