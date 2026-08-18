fc9f8d0801df8d459d21c16d302c4535a73f6a58
xmarre <54859656+xmarre@users.noreply.github.com>
2026-08-18 22:26:26 +0200

    Combine character consistency with output toggle fixes

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 import logging
 
 from . import image_conveyor as _image_conveyor
+from . import image_conveyor_drag_v2_ops as _image_conveyor_drag_v2_ops
 from .image_conveyor import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .image_conveyor_character_consistency import install_character_consistency
 from .image_conveyor_drag_v2_ops import register_drag_routes
 from .image_conveyor_library_ops import register_library_routes
 from .image_conveyor_server import register_routes
@@ -11,6 +13,7 @@ WEB_DIRECTORY = "./web"
 LOGGER = logging.getLogger(__name__)
 
 install_toggle_runtime(_image_conveyor)
+install_character_consistency(_image_conveyor_drag_v2_ops)
 
 
 def _register_route_group(name, register):
